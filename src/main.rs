6d4d01d2161521dc19247a674b72d4ce4b0acacd
taira-taiki <taira-taiki@kcgrp.jp>
2019-10-21 12:14:41 +0900

    add test method

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
+
+fn test_for(x: i32) {
+    println!("## test_for");
+    for y in 0..x {
+        println!("for_y: {}", y); // y: i32
+    }
+
+    for (i,j) in (5..10).enumerate() {
+        println!("for_enumerate: i = {} and j = {}", i, j);
+    }
+}
+
+fn test_loop(x: i32) {
+    println!("## test_loop");
+    let mut y = x;
+    loop {
+        y = y - 1;
+        println!("loop_y: {}", y);
+        if y < 0 {
+            break;
+        }
+    }
+}
+
 fn testtest(x: i32) -> i32 {
     println!("In testtest: {}", x);
     x + 1
 }
 
 fn test_array() {
+    println!("## test_array");
     let a = [1000.25; 20];
     println!("Array: {}", a[19]);
     println!("Array Length: {}", a.len());
+}
+
+fn test_slice() {
+    println!("## test_slice");
+    let a = [100; 20];
+    let complete = &a[..];
+    println!("complate.get {:?}",complete.get(1));
 }
 
 
@@ -44,21 +76,34 @@ fn test_func() {
     println!("Func availability: {}", z);
 }
 
-fn main() {
+fn test_func_ref() {
+    println!("## test_func_ref");
     let (foo, var) = (5, 4);
     let x: i32 = 3;
 
-    test_func();
     let y = testtest(x);
     println!("testtest returning value is : {}", y);
+
+    println!("You guessed: {}", foo);
+    println!("You guessed: {}", var);
+    println!("You guessed: {}", x);
+}
+
+fn main() {
+
+    test_loop(2);
+
+    test_for(5);
+
+    test_func();
+
+    test_func_ref();
 
     test_array();
 
     test_tuple();
 
-    println!("You guessed: {}", foo);
-    println!("You guessed: {}", var);
-    println!("You guessed: {}", x);
+    test_slice();
 
     test_if(2);
 }
